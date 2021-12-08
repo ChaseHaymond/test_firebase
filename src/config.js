273748a8b22d32466dbc7b6b9342d4ba8820b107
Chase <cdhaymond@gmail.com>
2021-12-07 23:47:31 -0700

    Updated css and fixed bingo logic

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+    apiKey: "AIzaSyAcFeYme2w88taJ2kHNp6oJCZ1l4w88q6Q",
+    authDomain: "chasesfirsttestapp.firebaseapp.com",
+    databaseURL: "https://chasesfirsttestapp-default-rtdb.firebaseio.com/"
+  };
+  
+  export default config;
--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ class Board extends React.Component {
   }
 
   getText() {
-    const text = ["Iwould be truly remiss", "Person on stand falls asleep", "Tender Mercies", "Odd use of Even", "Over time",
+    const text = ["I would be truly remiss", "Person on stand falls asleep", "Tender Mercies", "Odd use of Even", "Over time",
             "Race to pulpit", "Kid", "political", "Name dropper", "Fiber of my being",
           "Ill go up if you do", "TMI", "I know the church is true", "Thy son amen", "Kid with parent",
           "Bad joke", "The regular", "Scilence", "Metaphor", "Mission story", 
@@ -35,18 +35,22 @@ class Board extends React.Component {
   handleClick(i) {
     const squares = this.state.squares.slice();
     const classes = this.state.classes.slice();
-    if (calculateWinner(squares)) {
-      return;
-    }
 
-    squares[i] = this.state.xIsNext ? 'square2' : 'square';
-    classes[i] = classes[i] === 'square2' ? 'square' : 'square2'//this.state.xIsNext ? 'square2' : 'square'
+    squares[i] = classes[i] === 'square2' ? 'square' : 'square2';
+    classes[i] = classes[i] === 'square2' ? 'square' : 'square2';//this.state.xIsNext ? 'square2' : 'square'
+
+    console.log(i);
+    console.log(classes);
 
     this.setState({
       squares: squares,
       classes: classes,
       xIsNext: !this.state.xIsNext,
     });
+
+    if (calculateWinner(squares)) {
+      return true;
+    }
   }
 
   renderSquare(i) {
@@ -68,50 +72,44 @@ class Board extends React.Component {
 
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
+          
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(5)}
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+            {this.renderSquare(9)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(10)}
+            {this.renderSquare(11)}
+            {this.renderSquare(12)}
+            {this.renderSquare(13)}
+            {this.renderSquare(14)}
+            <span className="Winner">{status}</span>
+          </div>
+          <div className="board-row">
+            {this.renderSquare(15)}
+            {this.renderSquare(16)}
+            {this.renderSquare(17)}
+            {this.renderSquare(18)}
+            {this.renderSquare(19)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(20)}
+            {this.renderSquare(21)}
+            {this.renderSquare(22)}
+            {this.renderSquare(23)}
+            {this.renderSquare(24)}
+          </div>
         </div>
-        <div className="board-row">
-          {this.renderSquare(5)}
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-          {this.renderSquare(9)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(10)}
-          {this.renderSquare(11)}
-          {this.renderSquare(12)}
-          {this.renderSquare(13)}
-          {this.renderSquare(14)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(15)}
-          {this.renderSquare(16)}
-          {this.renderSquare(17)}
-          {this.renderSquare(18)}
-          {this.renderSquare(19)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(20)}
-          {this.renderSquare(21)}
-          {this.renderSquare(22)}
-          {this.renderSquare(23)}
-          {this.renderSquare(24)}
-        </div>
-        <div >{status}</div>
-        {status}
-        {status}
-        {status}
-        {status}
-        {status}
-        {status}
-        {status}
-      </div>
     );
   }
   }
